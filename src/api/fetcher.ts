5edc6676d4e06ab1ec7b5044e4a1262febce846b
victorreis <v.crisostomo.reis@gmail.com>
2023-11-08 18:29:23 -0300

    refactor: make env variable be accessed directly as it's now typed

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,5 +1,5 @@
 export const getData = async (path = '') => {
-  const url = `${import.meta.env['VITE_REACT_APP_API_BASE_URL']}/${path}`;
+  const url = `${import.meta.env.VITE_REACT_APP_API_BASE_URL}/${path}`;
   const res = await fetch(url);
   const json = await res.json();
 
