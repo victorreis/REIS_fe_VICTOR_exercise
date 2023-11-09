9bc2498e91c0a94011d1d967f7846951045e950a
victorreis <v.crisostomo.reis@gmail.com>
2023-11-09 01:02:58 -0300

    refactor: better organize constants

--- src/services/fetcher.ts
+++ src/services/fetcher.ts
@@ -1,5 +1,7 @@
+import { API_URL } from '@configs/constants';
+
 export const fetcher = async <T>(path = ''): Promise<T> => {
-  const url = `${import.meta.env.VITE_REACT_APP_API_BASE_URL}/${path}`;
+  const url = `${API_URL}/${path}`;
   const res = await fetch(url);
   const json = (await res.json()) as T;
 
