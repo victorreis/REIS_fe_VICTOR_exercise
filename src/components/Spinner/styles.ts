76778a3c89781011285e225339ce7ef4ba2b0fb3
victorreis <v.crisostomo.reis@gmail.com>
2023-11-09 20:33:13 -0300

    fix: make Spinner centralized

--- src/components/Spinner/styles.ts
+++ src/components/Spinner/styles.ts
@@ -10,6 +10,9 @@ from {
 `;
 
 export const SpinnerBody = styled.div`
+  position: absolute;
+  left: calc(50% - 2rem);
+  top: calc(50% - 2rem);
   height: 4rem;
   width: 4rem;
   border: 4px solid #d1d5db;
