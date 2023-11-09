05f53cef088df2d67dac074e5eeff31ea273c6d8
victorreis <v.crisostomo.reis@gmail.com>
2023-11-09 00:22:43 -0300

    refactor: move mappers to respective services

--- src/services/teams/index.ts
+++ src/services/teams/index.ts
@@ -1,5 +1,8 @@
+import { Column } from '@models/Column';
+import { Item } from '@models/Item';
 import { Team, TeamOverview } from '@models/Team';
 import { fetcher } from '@services/fetcher';
+import { hasAllValuesDefined } from '@utils/objects';
 
 const getAll = (): Promise<Team[]> => {
   return fetcher('teams');
@@ -9,7 +12,35 @@ const getOverviewById = (teamId: string): Promise<TeamOverview> => {
   return fetcher(`teams/${teamId}`);
 };
 
+const mapToItem = (team?: Partial<Team> | undefined): Item | undefined => {
+  if (!team || !hasAllValuesDefined(team)) return undefined;
+
+  const columns: Column[] = [
+    {
+      key: 'Name',
+      value: team.name,
+    },
+  ];
+
+  return {
+    id: team.id,
+    url: `/team/${team.id}`,
+    columns,
+    navigationProps: team,
+  };
+};
+
+const mapTeamsToItems = (teams?: Partial<Team>[]): Item[] => {
+  if (!teams) return [];
+
+  return teams
+    .map(mapToItem)
+    .filter((item): item is Item => item !== undefined);
+};
+
 export const TeamsService = {
   getAll,
   getOverviewById,
+  mapToItem,
+  mapTeamsToItems,
 };
--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -1,10 +1,85 @@
+import { Column } from '@models/Column';
+import { Item } from '@models/Item';
 import { User } from '@models/User';
 import { fetcher } from '@services/fetcher';
+import { hasAllValuesDefined } from '@utils/objects';
 
 const getById = (userId: string): Promise<User> => {
   return fetcher(`users/${userId}`);
 };
 
+const mapToColumns = (
+  user?: Partial<User> | undefined
+): Column[] | undefined => {
+  if (!user || !hasAllValuesDefined(user)) return undefined;
+
+  const columns: Column[] = [
+    {
+      key: 'Name',
+      value: `${user.firstName} ${user.lastName}`,
+    },
+    {
+      key: 'Display Name',
+      value: user.displayName,
+    },
+    {
+      key: 'Location',
+      value: user.location,
+    },
+  ];
+
+  return columns;
+};
+
+const mapToItem = (user?: Partial<User> | undefined): Item | undefined => {
+  if (!user || !hasAllValuesDefined(user)) return undefined;
+  const columns = mapToColumns(user);
+  if (!columns) return undefined;
+
+  return {
+    id: user.id,
+    url: `/user/${user.id}`,
+    columns,
+    navigationProps: user,
+  };
+};
+
+const mapUsersToItems = (users?: Partial<User>[] | undefined): Item[] => {
+  if (!users) return [];
+
+  return users
+    .map(mapToItem)
+    .filter((item): item is Item => item !== undefined);
+};
+
+const mapTeamLeadToColumns = (user?: Partial<User> | undefined): Column[] => {
+  if (!user || !hasAllValuesDefined(user)) return [];
+
+  const columns: Column[] = [
+    {
+      key: 'Team Lead',
+      value: '',
+    },
+    {
+      key: 'Name',
+      value: `${user.firstName} ${user.lastName}`,
+    },
+    {
+      key: 'Display Name',
+      value: user.displayName,
+    },
+    {
+      key: 'Location',
+      value: user.location,
+    },
+  ];
+  return columns;
+};
+
 export const UsersService = {
   getById,
+  mapToItem,
+  mapUsersToItems,
+  mapToColumns,
+  mapTeamLeadToColumns,
 };
