d770ddec93f095d430a6a1b504ee453893fbb791
victorreis <v.crisostomo.reis@gmail.com>
2023-11-10 18:22:46 -0300

    feat: create the Search component

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,50 @@
+import { SearchContainer } from '@components/Search/styles';
+
+export type SearchItem<T extends object> = {
+  id: string;
+  item: T;
+};
+
+export type SearchProps<T extends object> = {
+  value: string;
+  items: SearchItem<T>[];
+  keysToSearch?: string[];
+  onChange?: (newValue: string, items: T[]) => void;
+};
+
+const Search = <T extends object>({
+  value,
+  items,
+  keysToSearch,
+  onChange,
+}: SearchProps<T>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!onChange) return;
+
+    const newValue = e.target.value || '';
+    const filteredItems = items.filter((item) => {
+      return Object.entries(item.item).some(([key, itemValue]) => {
+        return (
+          ((keysToSearch && keysToSearch.includes(key)) || !keysToSearch) &&
+          !!itemValue &&
+          key !== 'id' &&
+          typeof itemValue === 'string' &&
+          String(itemValue).toLowerCase().includes(newValue.toLowerCase())
+        );
+      });
+    });
+
+    onChange(
+      newValue,
+      filteredItems.map((item) => item.item)
+    );
+  };
+
+  return (
+    <SearchContainer>
+      <input data-testid="search-input" onChange={handleChange} value={value} />
+    </SearchContainer>
+  );
+};
+
+export default Search;
---
+++ src/components/Search/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const SearchContainer = styled.div`
+  flex: 1;
+  margin: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
