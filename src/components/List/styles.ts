bdfb90c893dfc75cf3d58f500ec4ace02db156f2
victorreis <v.crisostomo.reis@gmail.com>
2023-11-10 23:51:57 -0300

    feat: make list responsible and use grid layout

--- src/components/List/styles.ts
+++ src/components/List/styles.ts
@@ -1,9 +1,24 @@
+import { screenBreakpointRanges, screenBreakpoints } from '@utils/screen';
 import styled from 'styled-components';
 
 export const ListContainer = styled.div`
-  flex: 1;
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
   justify-content: center;
-  align-items: center;
+  align-items: stretch;
+
+  @media only screen and (max-width: ${screenBreakpoints.sm}px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  @media ${screenBreakpointRanges.md} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media ${screenBreakpointRanges.lg} {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media ${screenBreakpointRanges.xl} {
+    grid-template-columns: repeat(4, 1fr);
+  }
 `;
